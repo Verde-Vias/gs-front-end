c13a2ff2ea9d340f52dc96c685a462f98754440b
juvascoli <jvascoli6@gmail.com>
2024-11-18 09:35:25 -0300

    deletando erro

--- verde-vias/src/app/sobre/page.tsx
+++ verde-vias/src/app/sobre/page.tsx
@@ -1,21 +1,7 @@
 
 export default function About() {
     return (
-        // <div className="sobre-nos-container flex items-center h-90vh justify-around">
-        //     <div className="text-center md:text-left ml-10 w-80">
-        //         <h1 className="text-2xl">A solução para o sucesso</h1>
-        //         <h2 className="text-2xl">O seu conserto de veículo aqui e agora!</h2>
-        //     </div>
-        //     <p className="w-[30%]">
-        //         A iniciativa busca superar o modelo tradicional e conservador do setor,
-        //         onde a interação física é um pré-requisito para qualquer diagnóstico ou
-        //         serviço. O diferencial trazido pelo projeto é o uso de Inteligência
-        //         Artificial para fornecer diagnósticos assertivos, ágeis e confiáveis,
-        //         garantindo ao cliente uma experiência denominada phygital — uma junção
-        //         do físico com o digital.
-        //     </p>
-        // </div>
-        <div className="h-screen">
+        <div className="h-screen flex items-center justify-center">
             <div className="mt-14 flex flex-col md:flex-row items-center justify-between text-center lg:justify-around">
                 <div className="text-center md:text-left ml-10 w-80">
                     <h1 className="text-2xl font-extrabold">VerdeVias </h1>
@@ -28,13 +14,11 @@ export default function About() {
                     ou seja, caminhos, corredores e sistemas de transporte que integram aspectos de sustentabilidade,
                     preservação ambiental e mobilidade urbana inteligente.
                 </p>
-
-
-
             </div>
         </div>
 
-
-
     )
+
+
+
 }
