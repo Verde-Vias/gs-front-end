8c40e7be6bf14349074ec1b0c41eeb3c78cb33fb
cadenasza <leonardocadena2005@gmail.com>
2024-11-22 19:18:21 -0300

    apagando aspas

--- verde-vias/src/app/sobre/page.tsx
+++ verde-vias/src/app/sobre/page.tsx
@@ -10,7 +10,7 @@ export default function About() {
                 <p className="lg:w-[50%] text-lg sm:w-[50%] sm:mt-6 sm:text-xl p-4">
                     A Verde<span className="text-verde">Vias</span> é uma iniciativa voltada para a criação e implementação de soluções sustentáveis em áreas urbanas,
                     com o objetivo de promover o desenvolvimento de infraestrutura ecológica e de transporte que tenha um impacto positivo no meio ambiente.
-                    O nome "Verde<span className="text-verde">Vias</span>" faz referência ao conceito de "vias verdes" —
+                    O nome Verde<span className="text-verde">Vias</span> faz referência ao conceito de vias verdes 
                     ou seja, caminhos, corredores e sistemas de transporte que integram aspectos de sustentabilidade,
                     preservação ambiental e mobilidade urbana inteligente.
                 </p>
